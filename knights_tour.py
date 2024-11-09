e079d5ae674c9710ca08c9b8f50e4bfe98c6c680
James-McTighe <james.mctighe97@gmail.com>
2024-11-09 16:10:06 -0500

    added input validation

--- knights_tour.py
+++ knights_tour.py
@@ -2,5 +2,15 @@ from typing import List, Tuple
 from collections import deque
 
 def knights_tour(n: int, start_x: int, start_y: int) -> List[Tuple[int, int]]:
-    # TODO: Implement
-    pass
+    input_constraints = [
+        n > 8,
+        n < 5,
+        start_x < 0,
+        start_x >= n,
+        start_y < 0,
+        start_y >= n
+    ]
+    
+    if any(input_constraints):
+        raise ValueError("Your input does not match the accepted values")
+
