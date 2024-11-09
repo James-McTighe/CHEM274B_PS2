8a30f7b21233a7765f8986fd6e9ce348a4d71485
James-McTighe <james.mctighe97@gmail.com>
2024-11-09 15:07:43 -0500

    finished lru cache

--- lru_cache.py
+++ lru_cache.py
@@ -27,12 +27,22 @@ class LRUCacheWithExpiration:
         self.timestamps = {}
 
     def get(self, key: int) -> int:
-        # TODO: Implement
-        pass
+        if key not in self.cache:
+            return -1
+        if (time.time() - self.timestamps[key]) > self.expiration_time:
+            self.cache.pop(key)
+            return -1
+        self.cache.move_to_end(key)
+        self.timestamps.update({key:time.time()})
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        # TODO: Implement
-        pass
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        self.timestamps.update({key:time.time()})
+        if len(self.cache) > self.capacity:
+            self.cache.popitem(last=False)
 
 class LRUCacheWithCallbacks:
     def __init__(self, capacity: int, on_eviction=None):
@@ -41,9 +51,17 @@ class LRUCacheWithCallbacks:
         self.on_eviction = on_eviction
 
     def get(self, key: int) -> int:
-        # TODO: Implement
-        pass
+        if key not in self.cache:
+            return -1
+        self.cache.move_to_end(key)
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
-        # TODO: Implement
-        pass
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            evicted_item_key, evicted_item_value = self.cache.popitem(last=False)
+            
+            if self.on_eviction != None:
+                self.on_eviction(evicted_item_key, evicted_item_value)
