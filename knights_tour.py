6978fca94dbf1ca4f01c801ebc9ff3965fc13dd4
James McTighe <james.mctighe97@gmail.com>
2024-11-09 22:33:55 -0500

    first draft

--- knights_tour.py
+++ knights_tour.py
@@ -1,16 +1,46 @@
 from typing import List, Tuple
 from collections import deque
 
+
 def knights_tour(n: int, start_x: int, start_y: int) -> List[Tuple[int, int]]:
-    input_constraints = [
-        n > 8,
-        n < 5,
-        start_x < 0,
-        start_x >= n,
-        start_y < 0,
-        start_y >= n
+    # Determines the validity of a particular move
+    def is_safe(board,x,y):
+        input_constraints = [
+        n < 8,
+        n > 5,
+        x > 0,
+        x <= n,
+        y > 0,
+        y <= n,
+        board[y][x] == -1
     ]
+        if all(input_constraints):
+            return True
+        else:
+            return False
     
-    if any(input_constraints):
-        raise ValueError("Your input does not match the accepted values")
+    board = [[-1 for x in range(n)]for y in range(n)]
+    # Each index is a valid move combination for a knight
+    move_x = [2, 1, -1, -2, -2, -1, 1, 2]
+    move_y = [1, 2, 2, 1, -1, -2, -2, -1]
+
+    board[start_x][start_y] = 0
+    pos = 1
+    results = []
+
+    def solveKTUtil(curr_x, curr_y):
+        nonlocal pos
+        if pos == n ** 2:
+            return
+        for i in range(8):
+            new_x = curr_x + move_x[i]
+            new_y = curr_y + move_y[i]
+            if is_safe(board,new_x, new_y):
+                pos += 1
+                board[new_x][new_y] = pos
+                results.append((new_x,new_y))
+                solveKTUtil(new_x,new_y)
+
+    solveKTUtil(start_x,start_y)
+    return results
 
