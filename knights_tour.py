367f1bab8ed39024ea55236c6a4f553b453a342b
James McTighe <james.mctighe97@gmail.com>
2024-11-09 22:42:52 -0500

    refactored, added backtracking step and got exercise to pass

--- knights_tour.py
+++ knights_tour.py
@@ -1,46 +1,40 @@
 from typing import List, Tuple
-from collections import deque
-
 
 def knights_tour(n: int, start_x: int, start_y: int) -> List[Tuple[int, int]]:
-    # Determines the validity of a particular move
-    def is_safe(board,x,y):
-        input_constraints = [
-        n < 8,
-        n > 5,
-        x > 0,
-        x <= n,
-        y > 0,
-        y <= n,
-        board[y][x] == -1
-    ]
-        if all(input_constraints):
-            return True
-        else:
-            return False
-    
-    board = [[-1 for x in range(n)]for y in range(n)]
-    # Each index is a valid move combination for a knight
+    # Determines if a move is valid
+    def is_valid(board, x, y):
+        return 0 <= x < n and 0 <= y < n and board[y][x] == -1
+
+    # Directions a knight can move (8 possibilities)
     move_x = [2, 1, -1, -2, -2, -1, 1, 2]
     move_y = [1, 2, 2, 1, -1, -2, -2, -1]
 
-    board[start_x][start_y] = 0
-    pos = 1
-    results = []
+    board = [[-1 for x in range(n)] for x in range(n)]
+    
+    board[start_y][start_x] = 0
+    results = [(start_x, start_y)]
 
-    def solveKTUtil(curr_x, curr_y):
-        nonlocal pos
+    def solver_utility(curr_x, curr_y, pos):
         if pos == n ** 2:
-            return
+            return True
+
         for i in range(8):
             new_x = curr_x + move_x[i]
             new_y = curr_y + move_y[i]
-            if is_safe(board,new_x, new_y):
-                pos += 1
-                board[new_x][new_y] = pos
-                results.append((new_x,new_y))
-                solveKTUtil(new_x,new_y)
 
-    solveKTUtil(start_x,start_y)
-    return results
+            # If the new position is valid and not visited
+            if is_valid(board, new_x, new_y):
+                board[new_y][new_x] = pos
+                results.append((new_x, new_y))
+                if solver_utility(new_x, new_y, pos + 1):
+                    return True
+                
+                board[new_y][new_x] = -1
+                results.pop()
 
+        return False
+
+    if solver_utility(start_x, start_y, 1):
+        return results
+    else:
+        return []
